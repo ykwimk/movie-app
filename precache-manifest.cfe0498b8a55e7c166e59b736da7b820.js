a09cf379ee7fed6b143aec599fc8fc8fd51b1be0
ykwimk <ykwimk@gmail.com>
2020-06-24 16:36:20 +0900

    Updates

---
+++ precache-manifest.cfe0498b8a55e7c166e59b736da7b820.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7a5c096623c91d2f71afb6e44b0b4c0",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "a3de1abf66c1fe4312d2",
+    "url": "/movie-app/static/css/main.602be52d.chunk.css"
+  },
+  {
+    "revision": "24b168e5da80ce41ad1a",
+    "url": "/movie-app/static/js/2.3f143b0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.3f143b0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3de1abf66c1fe4312d2",
+    "url": "/movie-app/static/js/main.49c41585.chunk.js"
+  },
+  {
+    "revision": "7cec98f03d005601501c",
+    "url": "/movie-app/static/js/runtime-main.9889d993.js"
+  }
+]);
